dcf790c29e908c88dd13cdc13ad6c91c00824651
8080473494 <65148268+8080473494@users.noreply.github.com>
2021-12-24 10:27:37 +0530

    Update pairSum.cpp

--- Lecture 10: Solving LeetCode Arrays/pairSum.cpp
+++ Lecture 10: Solving LeetCode Arrays/pairSum.cpp
@@ -5,13 +5,13 @@ vector<vector<int> > pairSum(vector<int> &arr, int s){
    
    for(int i=0;i<arr.size(); i++ )
    {
-   		for(int j = i+1; j<arr.size(); j++) {
-        	if(arr[i] +arr[j] == s)
+   	for(int j = i+1; j<arr.size(); j++) {
+        if(arr[i] +arr[j] == s)
             {
             	vector<int> temp;
                 temp.push_back(min(arr[i], arr[j]));
                 temp.push_back(max(arr[i], arr[j]));
-				ans.push_back(temp);
+		ans.push_back(temp);
             }
         }
    }
